93c01cd3dfad669a020646127a471f59fea8fbc0
OlenaKononenko23 <148655722+OlenaKononenko23@users.noreply.github.com>
2024-06-29 19:38:55 +0300

    1

---
+++ src/basic/3.ts
@@ -0,0 +1,8 @@
+let variant: string | number;
+variant = "OK";
+variant = "10";
+
+type EnableOrDisable = "enable" | "disable";
+let button: EnableOrDisable;
+button = "enable";
+button = "disable";
